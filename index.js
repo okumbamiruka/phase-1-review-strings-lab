d2b07cda28866404dea5395e1d1c8fe840f2c415
Chrispine Oburu <chrizoburu65@gmail.com>
2024-03-19 12:03:17 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const [firstName, lastName] = currentUser.split(' ');
+const firstNameInitial = firstName.charAt(0); // Get the first character of the first name
+const output = firstNameInitial + ' ' + lastName; // Concatenate the first character of the first name with the last name
+const shortGreeting = `Welcome, ${output}!`;
+
+console.log(shortGreeting)
